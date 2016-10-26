88dfc07eee4d0995e2c56535e513af8c7f286323
Adam Gajzlerowicz <adam.gajzlerowicz@performancehorizon.com>
2016-10-26 09:32:22 +0200

    Added Radium

--- app/components/Item.jsx
+++ app/components/Item.jsx
@@ -1,6 +1,8 @@
 import {render} from 'react-dom'
 import React, {Component} from 'react';
 import muiThemeable from 'material-ui/styles/muiThemeable';
+var color = require('color');
+var Radium = require('radium');
 
 
 const defaultTheme = {
@@ -9,16 +11,30 @@ const defaultTheme = {
     }
 };
 
+
+var styles = {
+    base: {
+        color: '#fff',
+        transition: 'width 2s, height 2s, background-color 2s, transform 2s',
+        ':hover': {
+            background: color('#0074d9').lighten(0.2).hexString(),
+            animation: 'x 3s ease 0s infinite',
+        }
+    }
+};
+
 const Item = ({...props, style}) => {
-
-    const elementStyle = Object.assign({}, defaultTheme, props.muiTheme.Item, style);
-
+    const elementStyle = Object.assign({}, defaultTheme.Item, props.muiTheme.Item, style);
+    console.log(elementStyle);
     return (
-        <div style={elementStyle}>
-            This is my styled component
+        <div
+            style={[
+                styles.base,
+                elementStyle
+            ]}>
+            {props.children}
         </div>
     );
-}
+};
 
-
-export default muiThemeable()(Item);
+export default muiThemeable()(Radium(Item));
--- app/index.jsx
+++ app/index.jsx
@@ -3,20 +3,25 @@ import {render} from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Item from './components/Item';
+var color = require('color');
+
 
 const applicationTheme = getMuiTheme({
-    Item: {
+    /*Item: {
         color: 'pink',
-        backgroundColor:'gray'
-    }
+        backgroundColor:'gray',
+        ':hover': {
+            background: color('#0074d9').lighten(0.2).hexString()
+        }
+    }*/
 });
 
 render(
     <MuiThemeProvider muiTheme={applicationTheme}>
-
         <div>
-            <Item />
-            <Item style={{color: 'blue', backgroundColor:'transparent'}}/>
+            <Item>I am a themed item item </Item>
+            <Item style={{color: 'blue', backgroundColor:'transparent'}}>
+                I am a custom styles item
+            </Item>
         </div>
-
     </MuiThemeProvider>, document.getElementById('app'));
